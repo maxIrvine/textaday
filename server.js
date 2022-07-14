51edcc55bcac8afa14ca1e58ba14d82742a31fdf
maxIrvine <max.irvine99@gmail.com>
2022-07-14 15:55:24 -0400

    test

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 const http = require("http");
-const hostname = 'localhost';
-const port = 3000;
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+http.createServer(function (req, res) {
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+  }).listen(8080); //the server object listens on port 8080
