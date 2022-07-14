ba50f4794d205ed80e6e5fc2b564850f6d80bdf5
maxIrvine <max.irvine99@gmail.com>
2022-07-14 15:57:23 -0400

    test2

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+console.log("test")
 const http = require("http");
+const hostname = 'localhost';
+const port = 3000;
 
 http.createServer(function (req, res) {
-    res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
-  }).listen(8080); //the server object listens on port 8080
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
