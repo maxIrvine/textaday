4d8a8708da895332429094e2663d29ad6d8ffc48
maxIrvine <max.irvine99@gmail.com>
2022-07-14 16:21:34 -0400

    added twilio

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-console.log("test")
-const http = require("http");
-const hostname = 'localhost';
-const port = 3000;
+require('dotenv').config();
 
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'Good morning, Max!',
+     from: '+16592224589',
+     to: '+14042818338'
+   })
+  .then(message => console.log(message.sid));
